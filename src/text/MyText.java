f4eadd10e1a5c6fd7d1b5efef52a49b886a05299
suseja <susannalagesson@gmail.com>
2021-11-05 22:27:08 +0100

    Add update2 1 part assignment Text.java

--- src/text/MyText.java
+++ src/text/MyText.java
@@ -10,6 +10,7 @@ public class MyText {
         Text myText = new Text(0, 0);
 
         System.out.println("Please enter your text. Print 'stop' when you want the program to end. ");
+        /*
         while (true) {
             String nextLine = scan.nextLine();
 
@@ -18,6 +19,14 @@ public class MyText {
             boolean userHasTypedStop = myText.lastLineContainedStop();
             if (userHasTypedStop) break;
         }
+        */
+
+        // condition is checked after 1 loop iteration
+        do {
+            String nextLine = scan.nextLine();
+            myText.addTextline(nextLine);
+        } while (!myText.lastLineContainedStop());
+        
         System.out.println("Number of characters: " + myText.getCharacters());
         System.out.println("Number of rows: " + myText.getNumberOfRows());
     }
